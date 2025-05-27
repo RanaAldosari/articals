9e5e4ac5570c4dacd9d74564bcb38fcff26f42ca
Rana Aldosari <rana@example.com>
2025-05-27 15:23:21 +0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,46 @@
 import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router'
 function Home() {
+  
 const[articl,setArticl] = useState([])
 
-useEffect(()=>{
-      axios.get(`https://jsonplaceholder.typicode.com/posts`)
-  .then( (response) =>{
-    setArticl(response.data)
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-},[])
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setArticl(data))
+    .catch(err => console.log('Failed to fetch data', err));
+}, []);
+
+
+// useEffect(()=>{
+// try {
+//         fetch("https://jsonplaceholder.typicode.com/posts")
+        
+//   .then((response.json()) =>{
+//     // 
+//     setArticl(response.data)
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+// } catch (error) {
+//   console.log(error)
+// }
+// },[])
 
   return (
     <>
     <div>
-{articl.map((item)=>(
-    <div>
+{articl.map((item,i)=>(
+    <div key={item.id}>
         {/* <p>{item.title}</p> */}
         <Link to={`/details/${item.id}`}>
         
-            <p>{item.title}</p>
+<p>{item.title}</p>
         
         </Link>
     </div>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Home from './Home'
 function Nav() {
   return (
     <div>
